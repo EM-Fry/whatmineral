36c0c7f87578287b136f4c01fa7618b14c67a005
Elliot-Fry <elioconpraro@gmail.com>
2026-08-25 10:33:40 +1000

    Fixed image overflow on mobiles maybe. Also added new minerals.

--- scripts.js
+++ scripts.js
@@ -1,26 +1,33 @@
 const imageDirectory = {
-    "Arsenopyrite": ["Screenshot 2026-08-10 094753.png", "Screenshot 2026-08-10 094800.png", "Screenshot 2026-08-10 094805.png"],
-    "Azurite": ["Screenshot 2026-08-10 100409.png", "Screenshot 2026-08-10 100435.png", "Screenshot 2026-08-10 100450.png", "Screenshot 2026-08-10 100607.png"],
-    "Bornite": ["Screenshot 2026-08-10 095438.png", "Screenshot 2026-08-10 095528.png", "Screenshot 2026-08-10 095606.png"],
-    "Cassiterite": ["Screenshot 2026-08-10 101612.png", "Screenshot 2026-08-10 101626.png", "Screenshot 2026-08-10 101944.png", "Screenshot 2026-08-10 102039.png", "Screenshot 2026-08-10 102115.png"],
-    "Chalcopyrite": ["Screenshot 2026-08-10 094312.png", "Screenshot 2026-08-10 094321.png"],
-    "Cobaltite": ["Screenshot 2026-08-10 101148.png", "Screenshot 2026-08-10 101201.png", "Screenshot 2026-08-10 101258.png", "Screenshot 2026-08-10 101331.png"],
-    "Cuprite": ["Screenshot 2026-08-10 095709.png", "Screenshot 2026-08-10 095719.png", "Screenshot 2026-08-10 095758.png"],
-    "Galena": ["Screenshot 2026-08-10 094448.png", "Screenshot 2026-08-10 094508.png", "Screenshot 2026-08-10 094515.png"],
-    "Gold": ["Screenshot 2026-08-10 095011.png", "Screenshot 2026-08-10 095016.png", "Screenshot 2026-08-10 095358.png"],
-    "Hematite": ["Screenshot 2026-08-10 104921.png", "Screenshot 2026-08-10 104930.png", "Screenshot 2026-08-10 104939.png", "Screenshot 2026-08-10 104948.png", "Screenshot 2026-08-10 105017.png", "Screenshot 2026-08-10 105050.png"],
-    "Ilmenite": ["Screenshot 2026-08-10 101429.png", "Screenshot 2026-08-10 101439.png", "Screenshot 2026-08-10 101538.png", "Screenshot 2026-08-10 101753.png"],
-    "Magnetite": ["Screenshot 2026-08-10 104600.png", "Screenshot 2026-08-10 104625.png", "Screenshot 2026-08-10 104638.png", "Screenshot 2026-08-10 104738.png", "Screenshot 2026-08-10 104812.png"],
-    "Malachite": ["Screenshot 2026-08-10 095953.png", "Screenshot 2026-08-10 100121.png", "Screenshot 2026-08-10 100240.png", "Screenshot 2026-08-10 100301.png"],
-    "Molybdenite": ["Screenshot 2026-08-10 100717.png", "Screenshot 2026-08-10 100727.png", "Screenshot 2026-08-10 100734.png", "Screenshot 2026-08-10 100747.png", "Screenshot 2026-08-10 100753.png"],
-    "Nickeline": ["Screenshot 2026-08-10 100845.png", "Screenshot 2026-08-10 100903.png", "Screenshot 2026-08-10 101013.png", "Screenshot 2026-08-10 101107.png"],
-    "Pentlandite": ["Screenshot 2026-08-10 094150.png", "Screenshot 2026-08-10 094209.png"],
-    "Pyrite": ["Screenshot 2026-08-10 093426.png", "Screenshot 2026-08-10 093500.png", "Screenshot 2026-08-10 093509.png"],
-    "Pyrrhotite": ["Screenshot 2026-08-10 093847.png", "Screenshot 2026-08-10 093859.png"],
-    "Sphalerite": ["Screenshot 2026-08-10 094604.png", "Screenshot 2026-08-10 094705.png", "Screenshot 2026-08-10 094717.png"],
-    "Stibnite": ["Screenshot 2026-08-10 094830.png", "Screenshot 2026-08-10 094841.png", "Screenshot 2026-08-10 094858.png", "Screenshot 2026-08-10 094902.png"],
-    "Wolframite": ["Screenshot 2026-08-10 104137.png", "Screenshot 2026-08-10 104208.png", "Screenshot 2026-08-10 104320.png", "Screenshot 2026-08-10 104514.png"]
+    "Arsenopyrite": ['Screenshot 2026-08-10 094753.png', 'Screenshot 2026-08-10 094800.png', 'Screenshot 2026-08-10 094805.png'],
+    "Azurite": ['Screenshot 2026-08-10 100409.png', 'Screenshot 2026-08-10 100435.png', 'Screenshot 2026-08-10 100450.png', 'Screenshot 2026-08-10 100607.png'],
+    "Barite": ['Screenshot 2026-08-24 155230.png', 'Screenshot 2026-08-24 155243.png', 'Screenshot 2026-08-24 155251.png', 'Screenshot 2026-08-24 155302.png'],
+    "Bornite": ['Screenshot 2026-08-10 095438.png', 'Screenshot 2026-08-10 095528.png', 'Screenshot 2026-08-10 095606.png'],
+    "Cassiterite": ['Screenshot 2026-08-10 101612.png', 'Screenshot 2026-08-10 101626.png', 'Screenshot 2026-08-10 101944.png', 'Screenshot 2026-08-10 102039.png', 'Screenshot 2026-08-10 102115.png'],
+    "Chalcopyrite": ['Screenshot 2026-08-10 094312.png', 'Screenshot 2026-08-10 094321.png'],
+    "Chromite": ['Screenshot 2026-08-24 154729.png', 'Screenshot 2026-08-24 154759.png', 'Screenshot 2026-08-24 154811.png'],
+    "Chrysocolla": ['Screenshot 2026-08-24 153722.png', 'Screenshot 2026-08-24 153801.png', 'Screenshot 2026-08-24 154037.png', 'Screenshot 2026-08-24 154113.png'],
+    "Cinnabar": ['Screenshot 2026-08-24 151719.png', 'Screenshot 2026-08-24 151752.png', 'Screenshot 2026-08-24 151801.png', 'Screenshot 2026-08-24 151814.png'],
+    "Cobaltite": ['Screenshot 2026-08-10 101148.png', 'Screenshot 2026-08-10 101201.png', 'Screenshot 2026-08-10 101258.png', 'Screenshot 2026-08-10 101331.png'],
+    "Copper": ['Screenshot 2026-08-24 160241.png', 'Screenshot 2026-08-24 160249.png', 'Screenshot 2026-08-24 160314.png', 'Screenshot 2026-08-24 160326.png'],
+    "Cuprite": ['Screenshot 2026-08-10 095709.png', 'Screenshot 2026-08-10 095719.png', 'Screenshot 2026-08-10 095758.png'],
+    "Galena": ['Screenshot 2026-08-10 094448.png', 'Screenshot 2026-08-10 094508.png', 'Screenshot 2026-08-10 094515.png'],
+    "Gold": ['Screenshot 2026-08-10 095011.png', 'Screenshot 2026-08-10 095016.png', 'Screenshot 2026-08-10 095358.png'],
+    "Hematite": ['Screenshot 2026-08-10 104921.png', 'Screenshot 2026-08-10 104930.png', 'Screenshot 2026-08-10 104939.png', 'Screenshot 2026-08-10 104948.png', 'Screenshot 2026-08-10 105017.png', 'Screenshot 2026-08-10 105050.png'],
+    "Ilmenite": ['Screenshot 2026-08-10 101429.png', 'Screenshot 2026-08-10 101439.png', 'Screenshot 2026-08-10 101538.png', 'Screenshot 2026-08-10 101753.png'],
+    "Magnesite": ['Screenshot 2026-08-24 154855.png', 'Screenshot 2026-08-24 154908.png', 'Screenshot 2026-08-24 154918.png', 'Screenshot 2026-08-24 154929.png'],
+    "Magnetite": ['Screenshot 2026-08-10 104600.png', 'Screenshot 2026-08-10 104625.png', 'Screenshot 2026-08-10 104638.png', 'Screenshot 2026-08-10 104738.png', 'Screenshot 2026-08-10 104812.png'],
+    "Malachite": ['Screenshot 2026-08-10 095953.png', 'Screenshot 2026-08-10 100121.png', 'Screenshot 2026-08-10 100240.png', 'Screenshot 2026-08-10 100301.png'],
+    "Molybdenite": ['Screenshot 2026-08-10 100717.png', 'Screenshot 2026-08-10 100727.png', 'Screenshot 2026-08-10 100734.png', 'Screenshot 2026-08-10 100747.png', 'Screenshot 2026-08-10 100753.png'],
+    "Nickeline": ['Screenshot 2026-08-10 100845.png', 'Screenshot 2026-08-10 100903.png', 'Screenshot 2026-08-10 101013.png', 'Screenshot 2026-08-10 101107.png'],
+    "Pentlandite": ['Screenshot 2026-08-10 094150.png', 'Screenshot 2026-08-10 094209.png'],
+    "Pyrite": ['Screenshot 2026-08-10 093426.png', 'Screenshot 2026-08-10 093500.png', 'Screenshot 2026-08-10 093509.png'],
+    "Pyrrhotite": ['Screenshot 2026-08-10 093847.png', 'Screenshot 2026-08-10 093859.png'],
+    "Sphalerite": ['Screenshot 2026-08-10 094604.png', 'Screenshot 2026-08-10 094705.png', 'Screenshot 2026-08-10 094717.png'],
+    "Stibnite": ['Screenshot 2026-08-10 094830.png', 'Screenshot 2026-08-10 094841.png', 'Screenshot 2026-08-10 094858.png', 'Screenshot 2026-08-10 094902.png'],
+    "Wolframite": ['Screenshot 2026-08-10 104137.png', 'Screenshot 2026-08-10 104208.png', 'Screenshot 2026-08-10 104320.png', 'Screenshot 2026-08-10 104514.png']
 };
+
 const minerals = Object.keys(imageDirectory);
 
 const imageframes  = ["image1", "image2"];
@@ -31,7 +38,33 @@ function changeImgs() {
     
 // -------PICK MINERALS-------- //
     // Minerals array:
-    const minerals = ["Arsenopyrite", "Azurite", "Bornite", "Cassiterite", "Chalcopyrite", "Cobaltite", "Cuprite", "Galena", "Gold", "Hematite", "Ilmenite", "Magnetite", "Malachite", "Molybdenite", "Nickeline", "Pentlandite", "Pyrite", "Pyrrhotite", "Sphalerite", "Stibnite", "Wolframite"];
+    const minerals = ["Arsenopyrite", 
+                        "Azurite", 
+                        "Barite", 
+                        "Bornite", 
+                        "Cassiterite", 
+                        "Chalcopyrite", 
+                        "Chromite", 
+                        "Chrysocolla", 
+                        "Cinnabar", 
+                        "Cobaltite", 
+                        "Copper", 
+                        "Cuprite", 
+                        "Galena", 
+                        "Gold", 
+                        "Hematite", 
+                        "Ilmenite", 
+                        "Magnesite", 
+                        "Magnetite", 
+                        "Malachite", 
+                        "Molybdenite", 
+                        "Nickeline", 
+                        "Pentlandite", 
+                        "Pyrite", 
+                        "Pyrrhotite", 
+                        "Sphalerite", 
+                        "Stibnite", 
+                        "Wolframite"];
     // Shuffle the mineral array using Fisher Yates method:
     for (let i = minerals.length -1; i > 0; i--) {
         let j = Math.floor(Math.random() * (i+1));
